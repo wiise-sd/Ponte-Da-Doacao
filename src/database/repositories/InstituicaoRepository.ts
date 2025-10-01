c18f2b8ecd2a1911e09e59b6f07adbd53140feb9
Carlos Sandro <sandro.dl17@hotmail.com>
2025-09-30 23:37:58 -0300

    feat: adicionar método findByCnpj à interface InstituicaoRepository

--- src/database/repositories/InstituicaoRepository.ts
+++ src/database/repositories/InstituicaoRepository.ts
@@ -4,4 +4,5 @@ export interface InstituicaoRepository {
   create(instituicao: Prisma.InstituicaoCreateInput): Promise<Instituicao>;
   findById(id: string): Promise<Instituicao | null>;
   findByEmail(email: string): Promise<Instituicao | null>;
+  findByCnpj(cnpj: string): Promise<Instituicao | null>;
 }
