9b2fb52bee593bc63aa76c3e25402d9f0c3b0565
Carlos Sandro <sandro.dl17@hotmail.com>
2025-10-02 00:04:59 -0300

    feat: adicionar rota para registro de instituição

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,23 @@
-import { Router } from "express";
-import UserController from "@/controllers/UserController";
+import { RequestHandler, Router, Request, Response } from "express";
+import { PrismaInstituicaoRepository } from "@/database/repositories/PrismaInstituicaoRepository";
+import { CreateInstituicaoUseCase } from "@/use-cases/registrar/create-instituicao";
+import { InstituicaoController } from "@/controllers/InstituicaoController";
 
 const routes = Router();
 
 routes.get("/health", (req, res) => res.json({ status: "ok" }));
 
-routes.post("/users", UserController.store);
+routes.post("/api/registrar", async (req: Request, res: Response) => {
+  const instituicaoRepository = new PrismaInstituicaoRepository();
+  const createInstituicaoUseCase = new CreateInstituicaoUseCase(
+    instituicaoRepository
+  );
+  const instituicaoController = new InstituicaoController(
+    createInstituicaoUseCase
+  );
+
+  const { status, body } = await instituicaoController.handle(req.body);
+  res.status(status).json(body);
+});
 
 export default routes;
