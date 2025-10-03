399398446b16411aafc000835b160902c2904483
Carlos Sandro <sandro.dl17@hotmail.com>
2025-10-02 22:54:47 -0300

    feat: criar schema de validação para instituição com campos obrigatórios

---
+++ src/schemas/InstituicaoSchema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { createErrorMap } from "zod-validation-error";
+
+// zod.config({
+//   customError: createErrorMap(),
+// });
+
+export const createInstituicaoSchema = z.object({
+  nome: z
+    .string({ required_error: "Nome é obrigatório" })
+    .min(3, "Nome deve ter pelo menos 3 caracteres"),
+  email: z
+    .string({ required_error: "Email é obrigatório" })
+    .email("Formato de email inválido"),
+  senha: z
+    .string({ required_error: "Senha é obrigatória" })
+    .min(6, "Senha deve ter pelo menos 6 caracteres"),
+  telefone: z
+    .string({ required_error: "Telefone é obrigatório" })
+    .min(10, "Telefone deve ter pelo menos 10 caracteres"),
+  cnpj: z
+    .string({ required_error: "CNPJ é obrigatório" })
+    .min(14, "CNPJ deve ter pelo menos 14 caracteres"),
+});
