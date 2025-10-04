3cd24879ebe37e17b132b9869f792329bc06ed40
Carlos Sandro <sandro.dl17@hotmail.com>
2025-10-03 21:27:04 -0300

    feat: restruturar a interface HttpRequest e melhorar tratamento de erros

--- src/controllers/InstituicaoController.ts
+++ src/controllers/InstituicaoController.ts
@@ -1,12 +1,20 @@
+import { createInstituicaoSchema } from "@/schemas/InstituicaoSchema";
+import {
+  CnpjAlreadyInUseError,
+  EmailAlreadyInUseError,
+} from "@/use-cases/errors/instituicao";
 import { CreateInstituicaoUseCase } from "@/use-cases/registrar/create-instituicao";
+import { ZodError } from "zod";
 
 interface HttpRequest {
-  nome: string;
-  cnpj: string;
-  telefone: string;
-  email: string;
-  senha: string;
-  descricao: string;
+  body: {
+    nome: string;
+    cnpj: string;
+    telefone: string;
+    email: string;
+    senha: string;
+    descricao: string;
+  };
 }
 
 export class InstituicaoController {
@@ -14,13 +22,47 @@ export class InstituicaoController {
 
   async handle(resquest: HttpRequest) {
     try {
-      const createdInstituicao = await this.instituicaoUseCase.handle(resquest);
+      const params = resquest.body;
+
+      await createInstituicaoSchema.parseAsync(params);
+
+      const createdInstituicao = await this.instituicaoUseCase.handle(
+        resquest.body
+      );
       return {
         status: 201,
         body: createdInstituicao,
       };
-    } catch (error) {
-      console.error("Error creating institution:", error);
+    } catch (err) {
+      console.error("Error creating institution:", err);
+
+      if (err instanceof ZodError) {
+        return {
+          status: 400,
+          body: {
+            message: err.errors[0].message,
+          },
+        };
+      }
+
+      if (err instanceof EmailAlreadyInUseError) {
+        return {
+          status: 409,
+          body: {
+            message: err.message,
+          },
+        };
+      }
+
+      if (err instanceof CnpjAlreadyInUseError) {
+        return {
+          status: 409,
+          body: {
+            message: err.message,
+          },
+        };
+      }
+
       return {
         status: 500,
         body: {
